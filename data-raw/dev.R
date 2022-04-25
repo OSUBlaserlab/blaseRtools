064f233776634aa757827fa8cb0c277500c28b33
blaserlab <bradley.blaser@osumc.edu>
2022-04-25 15:59:58 -0400

    version 0.0.0.9070

--- data-raw/dev.R
+++ data-raw/dev.R
@@ -12,7 +12,9 @@ purrr::walk(.x = list.files(path = "vignettes", pattern = "*.Rmd", full.names = 
 
 # commit and push
 gert::git_add("*")
-gert::git_commit("version 0.0.0.9069")
+gert::git_commit("version 0.0.0.9070")
 gert::git_push()
 
 renv::install("blaserlab/blaseRtools")
+
+
